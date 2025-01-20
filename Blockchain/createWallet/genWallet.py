6c528de2e8d4f0b3a38e04e79edfb7db28e4d9f6
xod <Vous@exemple.com>
2025-01-20 11:51:46 +0100

    folder purgeeee

--- Blockchain/createWallet/genWallet.py
+++
@@ -1,6 +0,0 @@
-from web3 import Account
-
-account = Account.create()
-
-print("Adresse Ethereum :", account.address)
-print("Clé privée :", account.key.hex())
--- TwoFactor/TwoFactory/myapp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import send_test_email
-
-urlpatterns = [
-    path('send_test_email/', send_test_email, name='send_test_email'),
-]
